aee5a3c424c31f9240f7fdecdfc5a64c9aead398
Katie Hoffman <ktahoffman@gmail.com>
2014-08-20 17:30:58 -0400

    does not account for when one pair is to the right but longer

--- lib/decode.rb
+++ lib/decode.rb
@@ -6,23 +6,24 @@ def decode(string)
 end
 
 def re_move_chars(message)
-  left = 0
+  left_i = 0
   max = message.length
-  right = max - 1
-  while left < max && right > left
-    current_character = message[left]
-
-    if current_character == message[right]
-      temp_arr = message[(left + 1)...right].split("")
-      if temp_arr.uniq == temp_arr # if there are no repeating characters
-        [left, right].each {|index| message.slice!(index) } # remove both characters
-        message << current_character # push character onto the end of the string
-        re_move_chars(message)
+  while left_i < max
+    current_character = message[left_i]
+    right_i = max - 1
+    while right_i > left_i
+      if current_character == message[right_i]
+        temp_arr = message[(left_i + 1)...right_i].split("")
+        if temp_arr.uniq == temp_arr # if there are no repeating characters
+          message.slice!(left_i) # remove left character
+          message.slice!(right_i - 1) # remove right character
+          message << current_character # push character onto the end of the string
+          return re_move_chars(message)
+        end
       end
+      right_i -= 1
     end
-
-    left += 1
-    right -= 1
+    left_i += 1
   end
   message
 end
